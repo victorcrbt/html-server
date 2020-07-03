9372c5671c454d469edac97c96baec1783e3aa42
Victor <madivcb@gmail.com>
2020-07-03 13:20:46 -0300

    created router

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const server = express();
+const publicFolder = path.resolve(__dirname, 'public');
+
+server.use('/assets', express.static(`${publicFolder}/assets`));
+
+server.get('/:file?', (req, res) => {
+  const { file } = req.params;
+
+  let fileToServe = file ? file.replace(/\.[^.]*$/, '') : 'index';
+
+  switch (fileToServe) {
+    case 'admin': {
+      fileToServe = 'admin_login';
+      break;
+    }
+
+    case 'login': {
+      fileToServe = 'admin_login';
+      break;
+    }
+
+    default:
+  }
+
+  return res.sendFile(`${publicFolder}/${fileToServe}.html`);
+});
+
+server.listen(5000);
